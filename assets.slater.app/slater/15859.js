22cb064af2be536845cbd60541a7cbc5c1386594
shamstajbir <25tajbir@gmail.com>
2025-09-04 15:03:25 +0600

    updated codes and the header and the logo

--- assets.slater.app/slater/15859.js
+++ assets.slater.app/slater/15859.js
@@ -1,9 +1,9 @@
 let paths = window.location.pathname.split("/");
-import("https://assets.slater.app/slater/15859/42282.js?v=917365");
+import("/assets.slater.app/slater/15859/42282.js");
 {
   const t = [""],
     s = paths[paths.length - 1],
     a = paths[paths.length - 2];
   (t.includes(s) || t.includes("detail_" + a) || t.includes(a + "/item")) &&
-    import("https://assets.slater.app/slater/15859/42283.js?v=875346");
+    import("/assets.slater.app/slater/15859/42283.js");
 }
